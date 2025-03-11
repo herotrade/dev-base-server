c9750ab97dce1df06d17d1c67be35e45cba101e0
dxx <dxx@xx.com>
2025-03-12 03:09:50 +0800

    qiutuleng/hyperf-dump-server 扩展配置

---
+++ config/autoload/dump-server.php
@@ -0,0 +1,10 @@
+<?php
+
+use function Hyperf\Support\env;
+
+return [
+    /*
+     * The host to use when listening for debug server connections.
+     */
+    'host' => env('DUMP_SERVER_HOST', 'tcp://127.0.0.1:9912'),
+];
